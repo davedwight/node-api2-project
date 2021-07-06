8c8a4b6997067663634b504ac2f71628ba6b34ff
davedwight <dpdwight@gmail.com>
2021-07-06 16:30:33 -0600

    Fetching all posts working

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it here
+const server = require('./api/server');
+const port = 4001;
+
+server.listen(4001, () => {
+    console.log(`*** Server running on http://localhost:${port}***`);
+})
